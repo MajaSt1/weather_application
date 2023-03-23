e1c475b1fc86d209df83932be83b2341222f528d
fms <fms@kmdpoland.pl>
2023-03-23 16:55:04 +0100

    create weather application

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://bestweather.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'bestweather.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'd4519a311amshd3ad9f8b40fd3f8p1a88bajsn07141a2dda41'
+};
